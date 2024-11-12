21c277ca85b6f56ccf56fd8c9fa96252e32abc15
Nikulochkin Andrey <nikulochkin.a@yandex.ru>
2024-11-12 19:29:34 +0300

    #1.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,10 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import './globals.css';
+import { Inter } from 'next/font/google';
+import '@/scss/global.scss';
 
 import { Header } from '@/components/shared';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -27,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={inter.className}>
         <Header />
         {children}
       </body>
--- components/shared/logo/index.tsx
+++ components/shared/logo/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-
+import { Lobster } from 'next/font/google';
 interface Props {
   className?: string;
+  classNameTitle?: string;
   nameLogo: string;
 }
 
-export const Logo: React.FC<Props> = ({ className, nameLogo }) => {
+const inter = Lobster({ weight: '400', subsets: ['latin'] });
+export const Logo: React.FC<Props> = ({ className, classNameTitle, nameLogo }) => {
   return (
-    <div className={className}>
-      <h1>{nameLogo}</h1>
+    <div className={`${className} ${inter.className}`}>
+      <h1 className={classNameTitle}>{nameLogo}</h1>
     </div>
   );
 };
